c71fb65388786753809a962de86c58de9f07503a
Tulun <jaykiraly@gmail.com>
2022-01-04 14:58:46 -0800

    render table
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import React from "react";
-import { Routes, Route, useLocation, useSearchParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Request from "./components/request";
 import Layout from "./components/layout";
 
 function App() {
-  const location = useLocation();
-  console.log("location", location);
-  const [searchParams] = useSearchParams();
-  console.log(
-    "SP",
-    searchParams,
-    searchParams.forEach((el) => console.log("el", el))
-  );
   return (
     <div className="App">
-      <h2>OO App</h2>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="request" element={<Request />} />
--- src/components/request/Request.tsx
+++ src/components/request/Request.tsx
@@ -1,6 +1,104 @@
+import Table, { ICell, Row } from "../table/Table";
+import { useSearchParams } from "react-router-dom";
+
+const headerCells: ICell[] = [
+  {
+    size: "xs",
+    value: "#",
+  },
+  {
+    size: "sm",
+    value: "Name",
+  },
+  {
+    size: "sm",
+    value: "Type",
+  },
+  {
+    size: "lg",
+    value: "Data",
+  },
+];
+
+const rows: Row[] = [
+  {
+    cells: [
+      {
+        size: "xs",
+        value: "0",
+      },
+      {
+        size: "sm",
+        value: "requester",
+      },
+      {
+        size: "sm",
+        value: "Address",
+      },
+      {
+        size: "lg",
+        value: (
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://etherscan.io/address/0x0000000000000000000000000000000000000000"
+          >
+            0x0000000000000000000000000000000000000000
+          </a>
+        ),
+      },
+    ],
+  },
+  {
+    cells: [
+      {
+        size: "xs",
+        value: "1",
+      },
+      {
+        size: "sm",
+        value: "identifier",
+      },
+      {
+        size: "sm",
+        value: "bytes32",
+      },
+      {
+        size: "lg",
+        value: "General KPI",
+      },
+    ],
+  },
+  {
+    cells: [
+      {
+        size: "xs",
+        value: "2",
+      },
+      {
+        size: "sm",
+        value: "timestamp",
+      },
+      {
+        size: "sm",
+        value: "uint256",
+      },
+      {
+        size: "lg",
+        value: "Nov 17 2021 23:00:00 (218817239812389)",
+      },
+    ],
+  },
+];
+
 const Request = () => {
-  console.log("rendering?");
-  return <div>Request Route</div>;
+  const [searchParams] = useSearchParams();
+
+  return (
+    <div>
+      <Table title={"Input Data"} headerCells={headerCells} rows={rows} />
+    </div>
+  );
 };
 
 export default Request;
