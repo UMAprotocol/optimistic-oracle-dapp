66b4afa60aac650e12b36827ee035a6e8cc9fe92
Tulun <jaykiraly@gmail.com>
2022-01-05 15:29:44 -0800

    Global styles stub
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Request from "./components/request";
 import Layout from "./components/layout";
-
+import GlobalStyles from "./components/global-styles";
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="request" element={<Request />} />
