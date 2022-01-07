7f86b8a619c5e1f0cdffd92b11612da7360f77de
Tulun <jaykiraly@gmail.com>
2022-01-07 12:19:18 -0800

    logo
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Request from "./components/request";
 import Layout from "./components/layout";
 import GlobalStyles from "./components/global-styles";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="request" element={<Request />} />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import Logo from "/public/logo.svg";
-import { Wrapper, MaxWidth } from "./Navbar.styled";
+import logo from "assets/logo.svg";
+import { Wrapper, MaxWidth, Navigation, ProductsButton } from "./Navbar.styled";
 
 export const Navbar: React.FC = () => {
   return (
     <Wrapper>
       <MaxWidth>
         <Link to="/">
-          <Logo />
+          <img src={logo} />
         </Link>
+        <Navigation>
+          <ProductsButton>Connect wallet</ProductsButton>
+        </Navigation>
       </MaxWidth>
     </Wrapper>
   );
