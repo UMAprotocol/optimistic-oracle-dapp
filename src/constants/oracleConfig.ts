e6ee5eae5e31e8990aac19049e7d32c187cfb4e5
Tulun <jaykiraly@gmail.com>
2022-01-13 09:40:20 -0800

    update type.
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/constants/oracleConfig.ts
+++ src/constants/oracleConfig.ts
@@ -1,13 +1,8 @@
-interface ChainConfig {
-  chainId: number;
-  multicall2Address?: string;
-  optimisticOracleAddress: string;
-  providerUrl: string;
-}
+import { oracle } from "@uma/sdk";
 
 // Ethereum config
 const ethChainId = 1;
-const ethChainConfig: ChainConfig = {
+const ethChainConfig: oracle.types.state.ChainConfig = {
   chainId: ethChainId,
   multicall2Address: "0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696",
   optimisticOracleAddress: "0xC43767F4592DF265B4a9F1a398B97fF24F38C6A6",
@@ -16,7 +11,7 @@ const ethChainConfig: ChainConfig = {
 
 // Polygon config
 const polygonChainId = 137;
-const polygonChainConfig: ChainConfig = {
+const polygonChainConfig: oracle.types.state.ChainConfig = {
   chainId: polygonChainId,
   optimisticOracleAddress: "0xBb1A8db2D4350976a11cdfA60A1d43f97710Da49",
   providerUrl: process.env.REACT_APP_PROVIDER_URL_137 ?? "",
