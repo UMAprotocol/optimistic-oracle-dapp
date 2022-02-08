c154d2553b40fea4b1d634326d182eae1be18a98
Tulun <jaykiraly@gmail.com>
2022-02-08 13:01:01 -0800

    add unicode range back to fonts
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- src/components/global-styles/GlobalStyles.tsx
+++ src/components/global-styles/GlobalStyles.tsx
@@ -10,6 +10,9 @@ export const typography = css`
     font-weight: normal;
     font-style: normal;
     font-display: swap;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
+      U+2215, U+FEFF, U+FFFD;
   }
 
   @font-face {
@@ -20,6 +23,9 @@ export const typography = css`
     font-weight: 500;
     font-style: normal;
     font-display: swap;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
+      U+2215, U+FEFF, U+FFFD;
   }
 
   @font-face {
@@ -29,6 +35,9 @@ export const typography = css`
     font-weight: 600;
     font-style: normal;
     font-display: swap;
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
+      U+2215, U+FEFF, U+FFFD;
   }
 `;
 const variables = css`
