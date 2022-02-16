283d3f8f17b5116f1f52d8b037bb7612209d2ff0
Tulun <jaykiraly@gmail.com>
2022-02-16 12:07:14 -0800

    remove comments from craco
    Signed-off-by: Tulun <jaykiraly@gmail.com>

--- craco.config.js
+++ craco.config.js
@@ -15,12 +15,6 @@ module.exports = {
       },
       resolve: {
         fallback: {
-          // process: require.resolve("process/browser"),
-          // zlib: require.resolve("browserify-zlib"),
-          // stream: require.resolve("stream-browserify"),
-          // util: require.resolve("util"),
-          // buffer: require.resolve("buffer"),
-          // asset: require.resolve("assert"),
           url: require.resolve("url"),
           fs: require.resolve("fs"),
           assert: require.resolve("assert"),
@@ -30,10 +24,6 @@ module.exports = {
           os: require.resolve("os-browserify/browser"),
           buffer: require.resolve("buffer"),
           stream: require.resolve("stream-browserify"),
-          // zlib: require.resolve("zlib"),
-          // tls: require.resolve("tls"),
-          // net: require.resolve("net"),
-          // dns: require.resolve("dns"),
         },
       },
       plugins: [
