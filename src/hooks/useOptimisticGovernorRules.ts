6cca7b94edd02f0d3b316e47d80903b36a6f9a65
abg4 <76416300+abg4@users.noreply.github.com>
2023-01-13 13:51:40 -0600

    improve: add explanation to zodiac proposals (#78)
    * improve: add explanation to zodiac proposals
    
    Signed-off-by: Alex Gaines <againes@umaproject.org>
    
    * convert explanation to string
    
    Signed-off-by: Alex Gaines <againes@umaproject.org>
    
    * Updates for review comments
    
    Signed-off-by: Alex Gaines <againes@umaproject.org>
    
    * Use compact abi format for optimistic governor
    
    Signed-off-by: Alex Gaines <againes@umaproject.org>
    
    Signed-off-by: Alex Gaines <againes@umaproject.org>

--- src/hooks/useOptimisticGovernorRules.ts
+++ src/hooks/useOptimisticGovernorRules.ts
@@ -2,31 +2,24 @@ import { useState, useEffect } from "react";
 import useClient from "hooks/useOracleClient";
 import useReader from "hooks/useOracleReader";
 import { ethers } from "ethers";
+import { toUtf8String } from "ethers/lib/utils";
 
 const ogAbi = [
-  {
-    inputs: [],
-    name: "rules",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
+  "function rules() view returns (string)",
+  "function proposeTransactions(tuple(address to, uint8 operation, uint256 value, bytes data)[] _transactions, bytes _explanation)",
 ];
 
 export function useOptimisticGovernorRules() {
   const { state } = useClient();
-  const { provider, requester } = useReader(state);
+  const { provider, requester, proposeTx } = useReader(state);
   const [rules, setRules] = useState<undefined | string>();
+  const [explanation, setExplanation] = useState<undefined | string>();
 
   useEffect(() => {
-    if (requester === undefined) return;
-    if (provider === undefined) return;
+    if (requester === undefined || provider === undefined) {
+      setRules(undefined);
+      return;
+    }
 
     async function getRules() {
       try {
@@ -41,5 +34,34 @@ export function useOptimisticGovernorRules() {
     getRules();
   }, [requester, provider]);
 
-  return { rules };
+  useEffect(() => {
+    async function getExplanation() {
+      if (
+        rules === undefined ||
+        provider === undefined ||
+        proposeTx === undefined
+      ) {
+        setExplanation(undefined);
+        return;
+      }
+
+      try {
+        const proposeData = await provider.getTransaction(proposeTx);
+        if (proposeData?.data !== undefined) {
+          const iface = new ethers.utils.Interface(ogAbi);
+          const decodedTransaction = iface.decodeFunctionData(
+            "proposeTransactions",
+            proposeData.data
+          );
+          setExplanation(toUtf8String(decodedTransaction[1]));
+        }
+      } catch (error) {
+        setExplanation(undefined);
+        console.error(`Get explanation failed: ${error}`);
+      }
+    }
+    getExplanation();
+  }, [proposeTx, provider, rules]);
+
+  return { rules, explanation };
 }
